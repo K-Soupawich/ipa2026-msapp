c26d45ab6929d0b557bfdf9daa0022c48f3e0b3c
KSoupawich <k.soupawich@gmail.com>
2026-09-23 01:54:30 +0700

    fix web5

--- worker/router_client.py
+++ worker/router_client.py
@@ -13,8 +13,7 @@ def get_interfaces(ip, username, password):
         "host": ip,
         "username": username,
         "password": password,
-        "session_log": "netmiko_session.log",
-        "disabled_algorithms": {"pubkeys": [], "kex": []},
+        "ssh_config_file": True,
     }
 
     with ConnectHandler(**device) as conn:
